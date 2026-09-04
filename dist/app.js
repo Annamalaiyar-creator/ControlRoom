917235e3e492b0e059d9cd22d2f783d295017022
Annamalaiyar <arannamalaiyar@gmail.com>
2026-09-04 17:54:52 +0530

    fix(iisnode): inject dist node_modules into NODE_PATH in app.js

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,13 @@
 // CommonJS wrapper for iisnode compatibility
+const path = require('path');
+const distModules = path.resolve(__dirname, 'node_modules');
+if (process.env.NODE_PATH) {
+  process.env.NODE_PATH = distModules + path.delimiter + process.env.NODE_PATH;
+} else {
+  process.env.NODE_PATH = distModules;
+}
+require('module').Module._initPaths();
+
 import('../server/index.js')
   .then(() => {
     console.log('[IISNODE] Control Room server loaded successfully via dynamic import.');
